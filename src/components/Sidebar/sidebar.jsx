75dd33129c2055679db036f07c7babb32da696cb
gayratovdv <javohirgayratov26@gmail.com>
2024-09-27 22:50:35 +0500

    something

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -13,16 +13,47 @@ const Sidebar = () => {
       </div>
       <ul className="sidebar__list">
         <li className={location.pathname === "/" ? "active" : ""}>
-          <i style={{margin: "0 10px 0 0"}} className="fa-solid fa-table"></i>
+          <i
+            style={
+              location.pathname === "/"
+                ? { margin: "0 10px 0 0", color: "white" }
+                : {}
+            }
+            className="fa-solid fa-table"
+          ></i>
           <Link to="/">Dashboard</Link>
         </li>
         <li className={location.pathname === "/members" ? "active" : ""}>
+          <i
+            style={
+              location.pathname === "/members"
+                ? { margin: "0 10px 0 0", color: "white" }
+                : {}
+            }
+            className="fas fa-chart-pie"
+          ></i>
           <Link to="/members">All Members</Link>
         </li>
         <li className={location.pathname === "/tasks" ? "active" : ""}>
+          <i
+            style={
+              location.pathname === "/tasks"
+                ? { margin: "0 10px 0 0", color: "white" }
+                : {}
+            }
+            className="fa-solid fa-clipboard-list"
+          ></i>
           <Link to="/tasks">Tasks</Link>
         </li>
         <li className={location.pathname === "/profile" ? "active" : ""}>
+          <i
+            style={
+              location.pathname === "/profile"
+                ? { margin: "0 10px 0 0", color: "white" }
+                : {}
+            }
+            className="fa-regular fa-address-card"
+          ></i>
           <Link to="/profile">Profile</Link>
         </li>
       </ul>
